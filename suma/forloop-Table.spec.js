5b031d8cccf06553e22149380d5f356a4e2bf623
RSDK <suma@MacBookAir.lan>
2025-10-15 20:52:42 +0530

    using forloop for table

---
+++ suma/forloop-Table.spec.js
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+
+test('has table', async ({ page }) => {
+  var url = 'https://testautomationpractice.blogspot.com/';
+  await page.goto(url);
+   console.log (await page.locator('//table[@id="taskTable"]//th['+column+']').textContent());
+  for(var row=1;row<=4;row++){
+   
+    for(var column=1;column<=5;column++){
+     
+     console.log (await page.locator('//table[@id="taskTable"]//tr['+row+']//td['+column+']').textContent());
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
